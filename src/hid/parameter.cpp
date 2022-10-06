89614fec20ce03bf311cede019520092d5ce40ec
Nick Donaldson <ndonald2@gmail.com>
2022-10-06 17:56:52 -0600

    Add ability to disable parameters

--- src/hid/parameter.cpp
+++ src/hid/parameter.cpp
@@ -3,33 +3,94 @@
 
 using namespace daisy;
 
-void Parameter::Init(AnalogControl input, float min, float max, Curve curve)
+static inline bool IsCloseTo(float in, float target, float epsilon = 0.001f)
 {
-    pmin_   = min;
-    pmax_   = max;
-    pcurve_ = curve;
-    in_     = input;
-    lmin_   = logf(min < 0.0000001f ? 0.0000001f : min);
-    lmax_   = logf(max);
+    return fabsf(in - target) <= epsilon;
+}
+
+void Parameter::Init(AnalogControl  input,
+                     float          min,
+                     float          max,
+                     Curve          curve,
+                     UpdateBehavior update_behavior)
+{
+    pmin_            = min;
+    pmax_            = max;
+    pcurve_          = curve;
+    in_              = input;
+    lmin_            = logf(min < 0.0000001f ? 0.0000001f : min);
+    lmax_            = logf(max);
+    val_             = 0.0f;
+    out_val_         = 0.0f;
+    raw_val_         = 0.0f;
+    d_raw_val_       = 0.0f;
+    enabled_         = true;
+    tracking_        = true;
+    update_behavior_ = update_behavior;
 }
 
 float Parameter::Process()
 {
+    float in = in_.Process();
+    raw_val_ = in_.GetRawFloat();
+
     switch(pcurve_)
     {
-        case LINEAR: val_ = (in_.Process() * (pmax_ - pmin_)) + pmin_; break;
-        case EXPONENTIAL:
-            val_ = in_.Process();
-            val_ = ((val_ * val_) * (pmax_ - pmin_)) + pmin_;
-            break;
-        case LOGARITHMIC:
-            val_ = expf((in_.Process() * (lmax_ - lmin_)) + lmin_);
-            break;
-        case CUBE:
-            val_ = in_.Process();
-            val_ = ((val_ * (val_ * val_)) * (pmax_ - pmin_)) + pmin_;
-            break;
+        case LINEAR: val_ = (in * (pmax_ - pmin_)) + pmin_; break;
+        case EXPONENTIAL: val_ = ((in * in) * (pmax_ - pmin_)) + pmin_; break;
+        case LOGARITHMIC: val_ = expf((in * (lmax_ - lmin_)) + lmin_); break;
+        case CUBE: val_ = ((in * (in * in)) * (pmax_ - pmin_)) + pmin_; break;
         default: break;
     }
-    return val_;
+
+    if(enabled_)
+    {
+        if(tracking_)
+        {
+            out_val_ = val_;
+        }
+        else
+        {
+            switch(update_behavior_)
+            {
+                case ON_CHANGE:
+                    if(!IsCloseTo(raw_val_, d_raw_val_))
+                    {
+                        tracking_ = true;
+                        out_val_  = val_;
+                    }
+                    break;
+                case PICKUP:
+                    if(IsCloseTo(raw_val_, d_raw_val_))
+                    {
+                        tracking_ = true;
+                        out_val_  = val_;
+                    }
+                    break;
+                default:
+                    tracking_ = true;
+                    out_val_  = val_;
+                    break;
+            }
+        }
+    }
+
+    return out_val_;
+}
+
+void Parameter::SetEnabled(bool enabled)
+{
+    if(!enabled && enabled_)
+    {
+        tracking_ = false;
+        if(update_behavior_ == PICKUP)
+        {
+            d_raw_val_ = raw_val_;
+        }
+    }
+    else if(enabled && !enabled_ && update_behavior_ == ON_CHANGE)
+    {
+        d_raw_val_ = raw_val_;
+    }
+    enabled_ = enabled;
 }
--- src/hid/parameter.h
+++ src/hid/parameter.h
@@ -8,7 +8,13 @@ namespace daisy
     @{
 */
 
-/**      Simple parameter mapping tool that takes a 0-1 input from an hid_ctrl. */
+/** Simple parameter mapping tool that takes a 0-1 input from an AnalogControl and adds the following
+ *  configurable post-processing:
+ *  - Map to a specific curve and output range
+ *  - Enables "virtaul parameters" with configurable update behavior
+ *      - Selectively enable/disable parameters mapped to the same AnalogControl (e.g. while holding a "shift" button)
+ *      - Configure how controls respond to raw input after becoming enabled again (via UpdateBehavior)
+ */
 class Parameter
 {
   public:
@@ -21,23 +27,41 @@ class Parameter
         CUBE,        /**< Cubic curve */
         LAST,        /**< Final enum element. */
     };
+
+    enum UpdateBehavior
+    {
+        ALWAYS,    /**< Always update value while enabled */
+        ON_CHANGE, /**< Update value only if changed after becoming enabled */
+        PICKUP, /**< Update value only if it reaches previous value after becoming enabled */
+        UPDATE_BEHAVIOR_LAST, /**< Final enum element */
+    };
+
     /** Constructor */
     Parameter() {}
     /** Destructor */
     ~Parameter() {}
 
     /** initialize a parameter using an hid_ctrl object.
-    \param input - object containing the direct link to a hardware control source.
-    \param min - bottom of range. (when input is 0.0)
-        \param max - top of range (when input is 1.0)
-    \param curve - the scaling curve for the input->output transformation.
+    \param input object containing the direct link to a hardware control source.
+    \param min bottom of range. (when input is 0.0)
+    \param max top of range (when input is 1.0)
+    \param curve the scaling curve for the input->output transformation.
+    \param update_behavior controls how previously disabled parameters update their value after becoming enabled
     */
-    void Init(AnalogControl input, float min, float max, Curve curve);
+    void Init(AnalogControl  input,
+              float          min,
+              float          max,
+              Curve          curve,
+              UpdateBehavior update_behavior = ALWAYS);
 
-    /** processes the input signal, this should be called at the samplerate of the hid_ctrl passed in.
+    /** Processes the input signal, this should be called at the samplerate of the AnalogControl passed in.
     \return  a float with the specified transformation applied.
     */
     float Process();
+
+    /** Set whether the parameter is enabled. If disabled, it will stop
+     *  updating its internal value on Process() */
+    void SetEnabled(bool enabled);
 
     /** 
     \return the current value from the parameter without processing another sample.
@@ -45,14 +69,17 @@ class Parameter
 
     the output of process in a local variable.
     */
-    inline float Value() { return val_; }
+    inline float Value() { return out_val_; }
 
   private:
-    AnalogControl in_;
-    float         pmin_, pmax_;
-    float         lmin_, lmax_; // for log range
-    float         val_;
-    Curve         pcurve_;
+    AnalogControl  in_;
+    float          pmin_, pmax_;
+    float          lmin_, lmax_; // for log range
+    float          val_, out_val_;
+    float          raw_val_, d_raw_val_;
+    bool           enabled_, tracking_;
+    Curve          pcurve_;
+    UpdateBehavior update_behavior_;
 };
 /** @} */
 } // namespace daisy
